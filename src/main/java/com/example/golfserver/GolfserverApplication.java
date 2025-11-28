a9405c3ca57cf391efd0aaa7872617b2a2f89ec8
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2025-11-28 16:18:45 +0900

    init commit

---
+++ src/main/java/com/example/golfserver/GolfserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.golfserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolfserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/golfserver/GolfserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.golfserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
